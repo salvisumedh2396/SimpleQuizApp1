92003482be83852ebe85963eded33666359d0cd8
Sumedh Salvi <scube2396@gmail.com>
2018-05-09 00:37:34 +0530

    First commit

--- app/src/main/java/com/example/android/simplequizapp/MainActivity.java
+++ app/src/main/java/com/example/android/simplequizapp/MainActivity.java
@@ -31,8 +31,7 @@ public class MainActivity extends AppCompatActivity {
 
     private int i;
     private Context context;
-    private int count =0;
-
+    private int count = 0;
 
 
     @Override
@@ -50,7 +49,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
 
-                Map<String,String> map = (Map) dataSnapshot.getValue();
+                Map<String, String> map = (Map) dataSnapshot.getValue();
 
                 for (i = 1; i <= 5; i++) {
                     //Will be changing this condition later to check if the question is a radiobox or checkbox or text for eg. if i=='checkbox' ||....so on
@@ -60,8 +59,8 @@ public class MainActivity extends AppCompatActivity {
                     String option3 = "option" + Integer.toString(i) + "c";
                     String option4 = "option" + Integer.toString(i) + "d";
                     String answer = "answer" + Integer.toString(i);
-                    String type = "type"+Integer.toString(i);
-                    String questionno = "Ques "+Integer.toString(i);
+                    String type = "type" + Integer.toString(i);
+                    String questionno = "Ques " + Integer.toString(i);
 
                     String question = map.get(quote1);
                     String op1 = map.get(option1);
@@ -71,7 +70,7 @@ public class MainActivity extends AppCompatActivity {
                     final String ans = map.get(answer);
                     String typ = map.get(type);
 
-                    System.out.println("+++++++++++++++"+typ);
+                    System.out.println("+++++++++++++++" + typ);
 
                     //Context context = getApplicationContext();
 
@@ -80,25 +79,25 @@ public class MainActivity extends AppCompatActivity {
                             LinearLayout.LayoutParams.WRAP_CONTENT
                     );
 
-                    p.setMargins(50,0,0,0);
+                    p.setMargins(50, 0, 0, 0);
 
                     LinearLayout.LayoutParams pc = new LinearLayout.LayoutParams(
                             LinearLayout.LayoutParams.FILL_PARENT,
                             LinearLayout.LayoutParams.WRAP_CONTENT
                     );
 
-                    pc.setMargins(100,20,100,20);
+                    pc.setMargins(100, 20, 100, 20);
 
                     TextView textView1 = new TextView(context);
                     textView1.setText(questionno);
                     textView1.setTextColor(Color.WHITE);
                     textView1.setTextSize(15);
-                    textView1.setPadding(0,50,0,0);
+                    textView1.setPadding(0, 50, 0, 0);
 
-                    Log.d("View","Start");
-                    try{
-                        linearLayout.addView(textView1,p);
-                    }catch(Exception e){
+                    Log.d("View", "Start");
+                    try {
+                        linearLayout.addView(textView1, p);
+                    } catch (Exception e) {
                         e.printStackTrace();
                     }
 
@@ -107,11 +106,11 @@ public class MainActivity extends AppCompatActivity {
                     textView.setText(question);
                     textView.setTextColor(Color.WHITE);
                     textView.setTextSize(20);
-                    textView.setPadding(0,0,0,100);
-                    Log.d("View","Start");
-                    try{
-                        linearLayout.addView(textView,p);
-                    }catch(Exception e){
+                    textView.setPadding(0, 0, 0, 100);
+                    Log.d("View", "Start");
+                    try {
+                        linearLayout.addView(textView, p);
+                    } catch (Exception e) {
                         e.printStackTrace();
                     }
 
@@ -119,16 +118,16 @@ public class MainActivity extends AppCompatActivity {
                     textView2.setText("Answer");
                     textView2.setTextColor(Color.WHITE);
                     textView2.setTextSize(20);
-                    textView2.setPadding(0,50,0,0);
-                    Log.d("View","Start");
-                    try{
-                        linearLayout.addView(textView2,p);
-                    }catch(Exception e){
+                    textView2.setPadding(0, 50, 0, 0);
+                    Log.d("View", "Start");
+                    try {
+                        linearLayout.addView(textView2, p);
+                    } catch (Exception e) {
                         e.printStackTrace();
                     }
 
                     if (typ.equals("radio")) {
-                        System.out.println("@@@@@@@@@@@@@2@@@@RadioBtn"+question+op1+op2+op3+op4+ans);
+                        System.out.println("@@@@@@@@@@@@@2@@@@RadioBtn" + question + op1 + op2 + op3 + op4 + ans);
                         final RadioGroup radioGroup = new RadioGroup(context);
 
 
@@ -136,30 +135,30 @@ public class MainActivity extends AppCompatActivity {
                         rdBtn1.setId(i);
                         rdBtn1.setText(op1);
                         rdBtn1.setTextColor(Color.WHITE);
-                        radioGroup.addView(rdBtn1,pc);
+                        radioGroup.addView(rdBtn1, pc);
 
                         final RadioButton rdBtn2 = new RadioButton(context);
-                        rdBtn2.setId(i*2);
+                        rdBtn2.setId(i * 2);
                         rdBtn2.setText(op2);
                         rdBtn2.setTextColor(Color.WHITE);
-                        radioGroup.addView(rdBtn2,pc);
+                        radioGroup.addView(rdBtn2, pc);
 
                         final RadioButton rdBtn3 = new RadioButton(context);
-                        rdBtn3.setId(i*3);
+                        rdBtn3.setId(i * 3);
                         rdBtn3.setText(op3);
                         rdBtn3.setTextColor(Color.WHITE);
-                        radioGroup.addView(rdBtn3,pc);
+                        radioGroup.addView(rdBtn3, pc);
 
                         final RadioButton rdBtn4 = new RadioButton(context);
                         rdBtn4.setText(op4);
-                        rdBtn3.setId(i*4);
+                        rdBtn3.setId(i * 4);
                         rdBtn4.setTextColor(Color.WHITE);
-                        radioGroup.addView(rdBtn4,pc);
+                        radioGroup.addView(rdBtn4, pc);
 
-                        linearLayout.addView(radioGroup,p);
+                        linearLayout.addView(radioGroup, p);
 
                         Button submit = new Button(context);
-                        submit.setId(i*5);
+                        submit.setId(i * 5);
                         submit.setText("Submit");
                         submit.setGravity(View.FOCUS_LEFT);
                         submit.setBackgroundResource(R.color.colorAccent);
@@ -168,12 +167,12 @@ public class MainActivity extends AppCompatActivity {
                                 LinearLayout.LayoutParams.WRAP_CONTENT
                         );
 
-                        params.setMargins(100,20,20,50);
+                        params.setMargins(100, 20, 20, 50);
 
 
                         submit.setTextColor(Color.WHITE);
 
-                        linearLayout.addView(submit,params);
+                        linearLayout.addView(submit, params);
                         //System.out.println("ANSWER OF THE DAY 1: "+question);
 
                         submit.setOnClickListener(new View.OnClickListener() {
@@ -183,10 +182,10 @@ public class MainActivity extends AppCompatActivity {
                                 rdBtn2.setEnabled(false);
                                 rdBtn3.setEnabled(false);
                                 rdBtn4.setEnabled(false);
-                                for(int i=0; i<radioGroup.getChildCount(); i++) {
+                                for (int i = 0; i < radioGroup.getChildCount(); i++) {
                                     RadioButton btn = (RadioButton) radioGroup.getChildAt(i);
                                     String selectedOption = (String) btn.getText();
-                                    if(selectedOption.equals(ans)) {
+                                    if (selectedOption.equals(ans)) {
                                         count++;
                                     }
                                 }
@@ -196,39 +195,36 @@ public class MainActivity extends AppCompatActivity {
                         });
 
 
-                    }
-                    else if (typ.equals("checkbox"))
-                    {
-                        String answer1 = "answer" + Integer.toString(i)+"a";
-                        String answer2 = "answer" + Integer.toString(i)+"b";
+                    } else if (typ.equals("checkbox")) {
+                        String answer1 = "answer" + Integer.toString(i) + "a";
+                        String answer2 = "answer" + Integer.toString(i) + "b";
                         final String ans1 = map.get(answer1);
                         final String ans2 = map.get(answer2);
-                        System.out.println("_-__-___-----CheckboxBtn"+question+op1+op2+op3+op4+ans1+ans2);
-
+                        System.out.println("_-__-___-----CheckboxBtn" + question + op1 + op2 + op3 + op4 + ans1 + ans2);
 
 
                         final CheckBox chbx1 = new CheckBox(context);
                         chbx1.setText(op1);
                         chbx1.setTextColor(Color.WHITE);
-                        linearLayout.addView(chbx1,pc);
+                        linearLayout.addView(chbx1, pc);
 
                         final CheckBox chbx2 = new CheckBox(context);
                         chbx2.setText(op2);
                         chbx2.setTextColor(Color.WHITE);
-                        linearLayout.addView(chbx2,pc);
+                        linearLayout.addView(chbx2, pc);
 
                         final CheckBox chbx3 = new CheckBox(context);
                         chbx3.setText(op3);
                         chbx3.setTextColor(Color.WHITE);
-                        linearLayout.addView(chbx3,pc);
+                        linearLayout.addView(chbx3, pc);
 
                         final CheckBox chbx4 = new CheckBox(context);
                         chbx4.setText(op4);
                         chbx4.setTextColor(Color.WHITE);
-                        linearLayout.addView(chbx4,pc);
+                        linearLayout.addView(chbx4, pc);
 
                         Button submit = new Button(context);
-                        submit.setId(i*5);
+                        submit.setId(i * 5);
                         submit.setText("Submit");
                         submit.setGravity(View.FOCUS_LEFT);
                         submit.setBackgroundResource(R.color.colorAccent);
@@ -237,12 +233,12 @@ public class MainActivity extends AppCompatActivity {
                                 LinearLayout.LayoutParams.WRAP_CONTENT
                         );
 
-                        params.setMargins(100,20,20,50);
+                        params.setMargins(100, 20, 20, 50);
 
 
                         submit.setTextColor(Color.WHITE);
 
-                        linearLayout.addView(submit,params);
+                        linearLayout.addView(submit, params);
                         //System.out.println("ANSWER OF THE DAY 1: "+question);
 
                         submit.setOnClickListener(new View.OnClickListener() {
@@ -253,34 +249,30 @@ public class MainActivity extends AppCompatActivity {
                                 chbx2.setEnabled(false);
                                 chbx3.setEnabled(false);
                                 chbx4.setEnabled(false);
-                                if (chbx1.isChecked()){
+                                if (chbx1.isChecked()) {
                                     String text1 = chbx1.getText().toString();
-                                    if(text1.equals(ans1) || text1.equals(ans2))
-                                    {
+                                    if (text1.equals(ans1) || text1.equals(ans2)) {
                                         count++;
                                     }
 
                                 }
-                                if (chbx2.isChecked()){
+                                if (chbx2.isChecked()) {
                                     String text1 = chbx2.getText().toString();
-                                    if(text1.equals(ans1) || text1.equals(ans2))
-                                    {
+                                    if (text1.equals(ans1) || text1.equals(ans2)) {
                                         count++;
                                     }
 
                                 }
-                                if (chbx3.isChecked()){
+                                if (chbx3.isChecked()) {
                                     String text1 = chbx3.getText().toString();
-                                    if(text1.equals(ans1) || text1.equals(ans2))
-                                    {
+                                    if (text1.equals(ans1) || text1.equals(ans2)) {
                                         count++;
                                     }
 
                                 }
-                                if (chbx4.isChecked()){
+                                if (chbx4.isChecked()) {
                                     String text1 = chbx4.getText().toString();
-                                    if(text1.equals(ans1) || text1.equals(ans2))
-                                    {
+                                    if (text1.equals(ans1) || text1.equals(ans2)) {
                                         count++;
                                     }
 
@@ -288,9 +280,7 @@ public class MainActivity extends AppCompatActivity {
                             }
                         });
 
-                    }
-                    else if(typ.equals("textinput"))
-                    {
+                    } else if (typ.equals("textinput")) {
                         final EditText edtxt1 = new EditText(context);
                         edtxt1.setTextColor(Color.WHITE);
                         edtxt1.setTextSize(20);
@@ -302,7 +292,7 @@ public class MainActivity extends AppCompatActivity {
                         }
 
                         Button submit = new Button(context);
-                        submit.setId(i*5);
+                        submit.setId(i * 5);
                         submit.setText("Submit");
                         submit.setGravity(View.FOCUS_LEFT);
                         submit.setBackgroundResource(R.color.colorAccent);
@@ -311,11 +301,11 @@ public class MainActivity extends AppCompatActivity {
                                 LinearLayout.LayoutParams.WRAP_CONTENT
                         );
 
-                        params.setMargins(100,20,20,50);
+                        params.setMargins(100, 20, 20, 50);
 
 
                         submit.setTextColor(Color.WHITE);
-                        linearLayout.addView(submit,params);
+                        linearLayout.addView(submit, params);
                         //System.out.println("ANSWER OF THE DAY 1: "+question);
 
                         submit.setOnClickListener(new View.OnClickListener() {
@@ -323,8 +313,7 @@ public class MainActivity extends AppCompatActivity {
                             public void onClick(View v) {
                                 edtxt1.setEnabled(false);
                                 String text = edtxt1.getText().toString();
-                                if(text.equals(ans))
-                                {
+                                if (text.equals(ans)) {
                                     count++;
                                 }
                             }
@@ -350,13 +339,13 @@ public class MainActivity extends AppCompatActivity {
 
                 submit.setTextColor(Color.WHITE);
 
-                linearLayout.addView(submit,params);
+                linearLayout.addView(submit, params);
                 //System.out.println("ANSWER OF THE DAY 1: "+question);
 
                 submit.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        CharSequence text = "Your score is ("+Integer.toString(count)+"/6) !!";
+                        CharSequence text = "Your score is (" + Integer.toString(count) + "/6) !!";
                         int duration = Toast.LENGTH_SHORT;
                         Toast toast = Toast.makeText(context, text, duration);
                         toast.show();
@@ -370,8 +359,6 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
-
-
 
 
     }
